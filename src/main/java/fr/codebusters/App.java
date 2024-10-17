e16c65b9d720df405feb0410a2638548a073a1fc
SabiMo <sabi.mohamed29@gmail.com>
2024-10-17 16:38:39 +0200

    Initialisation du projet

---
+++ src/main/java/fr/codebusters/App.java
@@ -0,0 +1,13 @@
+package fr.codebusters;
+
+/**
+ * Point d'entrée de l'application.
+ */
+public class App {
+    /**
+     * Méthode qui lance l'application.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
